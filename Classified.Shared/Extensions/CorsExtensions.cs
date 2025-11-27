94abf00a5d92593f281445554f41abce36eecebe
Timmmofey <tim.izmail@mail.ru>
2025-11-28 00:23:58 +0300

    Вынесена логика корсов в отдельный общий файл

---
+++ Classified.Shared/Extensions/CorsExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Classified.Shared.Extensions
+{
+    public static class CorsExtensions
+    {
+        public static IServiceCollection AddDefaultCors (this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowFrontend", policy =>
+                {
+                    policy.WithOrigins("http://localhost:3000")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowCredentials();
+                });
+            });
+
+            return services;
+        }
+    }
+}
